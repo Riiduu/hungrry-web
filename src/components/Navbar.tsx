bbeb86c4a301eb4077899db91d248cf9e7b700d6
soapman <jelinsksk@gmail.com>
2025-01-19 12:45:31 +0000

    Navbar ui finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 // Mui icons
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import { FavoriteBorder } from '@mui/icons-material';
 
 const Navbar = () => {
     return (
-        <div className="bg-[var(--col-primary)] flex flex-col w-full p-5 sm:justify-between">
+        <div className="bg-[var(--col-primary)] flex flex-col w-full p-5 sm:justify-between sm:flex-row items-center">
             <div className="flex flex-row  justify-between items-center sm:justify-start">
                 <div className="cursor-pointer sm:hidden">
                     <MenuIcon fontSize="large"/>
@@ -14,10 +15,16 @@ const Navbar = () => {
                 {/* Just an empty div for the spacing. Probably a better way out there, but this works, so why not */}
                 <div></div>
             </div>
+            <div className='space-x-3'>
+                {/** TODO: Figure out how to make them thinner */}
+                <SearchIcon fontSize='large' className='text-white cursor-pointer'/>
+                <FavoriteBorder fontSize='large' className='text-white cursor-pointer'/>
+            </div>
             <form className="flex flex-col w-full pt-5 sm:hidden">
                 <div className="flex flex-row items-center bg-white pl-3 rounded-lg">
                     <SearchIcon />
-                    <input className="p-2 rounded-lg w-full focus:outline-none" onSubmit={() => console.log("Your mother")} placeholder="List ingredients" type="text"/>
+                    {/** TODO: Add input functionality */}
+                    <input className="p-2 rounded-lg w-full focus:outline-none" onSubmit={() => {}} placeholder="List ingredients" type="text"/>
                 </div>
             </form>
         </div>

bbc7a0ddaa36a5b12d1991e4248c72000006d4cf
soapman <jelinsksk@gmail.com>
2025-01-19 12:49:23 +0000

    Slight sizing adjustments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,28 +5,30 @@ import { FavoriteBorder } from '@mui/icons-material';
 
 const Navbar = () => {
     return (
-        <div className="bg-[var(--col-primary)] flex flex-col w-full p-5 sm:justify-between sm:flex-row items-center">
-            <div className="flex flex-row  justify-between items-center sm:justify-start">
-                <div className="cursor-pointer sm:hidden">
-                    <MenuIcon fontSize="large"/>
-                </div>
+        <div className="bg-[var(--col-primary)] w-full p-5">
+            <div className='max-w-4xl mx-auto flex flex-col sm:justify-between sm:flex-row items-center'>
+                <div className="flex flex-row  justify-between items-center sm:justify-start">
+                    <div className="cursor-pointer sm:hidden">
+                        <MenuIcon fontSize="large"/>
+                    </div>
 
-                <h1 className="text-3xl font-bold cursor-pointer">Hungrry</h1>
-                {/* Just an empty div for the spacing. Probably a better way out there, but this works, so why not */}
-                <div></div>
-            </div>
-            <div className='space-x-3'>
-                {/** TODO: Figure out how to make them thinner */}
-                <SearchIcon fontSize='large' className='text-white cursor-pointer'/>
-                <FavoriteBorder fontSize='large' className='text-white cursor-pointer'/>
-            </div>
-            <form className="flex flex-col w-full pt-5 sm:hidden">
-                <div className="flex flex-row items-center bg-white pl-3 rounded-lg">
-                    <SearchIcon />
-                    {/** TODO: Add input functionality */}
-                    <input className="p-2 rounded-lg w-full focus:outline-none" onSubmit={() => {}} placeholder="List ingredients" type="text"/>
+                    <h1 className="text-3xl font-bold cursor-pointer">Hungrry</h1>
+                    {/* Just an empty div for the spacing. Probably a better way out there, but this works, so why not */}
+                    <div></div>
                 </div>
-            </form>
+                <div className='space-x-3'>
+                    {/** TODO: Figure out how to make them thinner */}
+                    <SearchIcon fontSize='large' className='text-white cursor-pointer'/>
+                    <FavoriteBorder fontSize='large' className='text-white cursor-pointer'/>
+                </div>
+                <form className="flex flex-col w-full pt-5 sm:hidden">
+                    <div className="flex flex-row items-center bg-white pl-3 rounded-lg">
+                        <SearchIcon />
+                        {/** TODO: Add input functionality */}
+                        <input className="p-2 rounded-lg w-full focus:outline-none" onSubmit={() => {}} placeholder="List ingredients" type="text"/>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import Navbar from '../components/Navbar'
 
 const HomePage = () => {
     return (
-        <div className="max-width-2xl mx-auto flex flex-col justify-center items-center">
+        <div className="max-width-4xl mx-auto flex flex-col justify-center items-center">
             <Navbar/>
         </div>
     );

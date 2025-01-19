bbdca4be52409a757f098c120760dc480049a41d
soapman <jelinsksk@gmail.com>
2025-01-19 14:25:22 +0200

    I think I know what's happening

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,26 @@
+// Mui icons
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
 const Navbar = () => {
     return (
-        <>
-        </>
+        <div className="bg-[var(--col-primary)] flex flex-col w-full p-5 sm:justify-between">
+            <div className="flex flex-row  justify-between items-center sm:justify-start">
+                <div className="cursor-pointer sm:hidden">
+                    <MenuIcon fontSize="large"/>
+                </div>
+
+                <h1 className="text-3xl font-bold cursor-pointer">Hungrry</h1>
+                {/* Just an empty div for the spacing. Probably a better way out there, but this works, so why not */}
+                <div></div>
+            </div>
+            <form className="flex flex-col w-full pt-5 sm:hidden">
+                <div className="flex flex-row items-center bg-white pl-3 rounded-lg">
+                    <SearchIcon />
+                    <input className="p-2 rounded-lg w-full focus:outline-none" onSubmit={() => console.log("Your mother")} placeholder="List ingredients" type="text"/>
+                </div>
+            </form>
+        </div>
     )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar'
 const HomePage = () => {
     return (
         <div className="max-width-2xl mx-auto flex flex-col justify-center items-center">
-            <h1>Your motehr</h1>
             <Navbar/>
         </div>
     );
